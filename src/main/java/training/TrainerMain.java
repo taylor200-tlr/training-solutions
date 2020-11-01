df5bcd59693c2dae58bdbe720a9d1a173cbf1602
taylor200-tlr <taylor200@freemail.hu>
2020-11-01 19:43:07 +0100

    java osztályok konstruktorral plusz

--- src/main/java/training/TrainerMain.java
+++ src/main/java/training/TrainerMain.java
@@ -1,15 +1,19 @@
 package training;
 
+import java.util.Scanner;
+
 public class TrainerMain {
 
     public static void main(String[] args) {
-        Trainer trainer = new Trainer("John Doe", 1980);
-        Trainer anotherTrainer = new Trainer("Jack Doe", 1974);
+        System.out.println("Mi a neved?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine() ;
 
-        System.out.println(trainer.getName());
-        System.out.println(trainer.getYearOfBirth());
-        System.out.println(anotherTrainer.getName());
-        System.out.println(anotherTrainer.getYearOfBirth());
+        System.out.println("Mikor születtél?");
+        int yearOfBirth = scanner.nextInt();
+
+        Trainer trainer = new Trainer(name, yearOfBirth);
+        System.out.println(trainer.getNameAndYearOfBirth());
 
     }
 }
