b225c4dc0298d021c1793d31080d72a16520ba78
taylor200-tlr <taylor200@freemail.hu>
2020-11-01 21:15:49 +0100

    Product and price

---
+++ src/main/java/classstructure/BankAccount.java
@@ -0,0 +1,32 @@
+package classstructure;
+
+public class BankAccount {
+
+    private String accountNumber;
+    private String owner;
+    private int balance;
+    private int amount;
+
+    public BankAccount(String accountNumber, String owner, int balance) {
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public void deposit(int amount) {
+        balance = balance + amount;
+    }
+
+    public void withdraw(int amount) {
+        balance = balance - amount;
+    }
+
+    public String getInfo() {
+        return owner + "(" + accountNumber + "):" + balance + " Ft";
+    }
+
+    public void transfer(BankAccount masikra, int amount) {
+        this.withdraw(amount);
+        masikra.deposit(amount);
+    }
+}
---
+++ src/main/java/classstructure/BankMain.java
@@ -0,0 +1,10 @@
+package classstructure;
+
+import java.util.Scanner;
+
+public class BankMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
