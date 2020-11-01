7e7946f99248c2fc7381a789e947191efa530f67
taylor200-tlr <taylor200@freemail.hu>
2020-11-01 19:18:35 +0100

    java osztályok konstruktorral

--- src/main/java/training/Trainer.java
+++ src/main/java/training/Trainer.java
@@ -4,6 +4,13 @@ public class Trainer {
     private String name;
     private int yearOfBirth;
 
+    public Trainer(String name, int yearOfBirth) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+}
+
     public String getName() {
         return name;
     }
--- src/main/java/training/TrainerMain.java
+++ src/main/java/training/TrainerMain.java
@@ -3,24 +3,13 @@ package training;
 public class TrainerMain {
 
     public static void main(String[] args) {
-        Trainer trainer = new Trainer();
-        trainer.setName("Jhohn Doe");
-        trainer.setYearOfBirth(1974);
+        Trainer trainer = new Trainer("John Doe", 1980);
+        Trainer anotherTrainer = new Trainer("Jack Doe", 1974);
+
         System.out.println(trainer.getName());
         System.out.println(trainer.getYearOfBirth());
-
-        Trainer anotherTrainer = new Trainer();
-        anotherTrainer.setName("Jack Doe");
-        anotherTrainer.setYearOfBirth(1980);
         System.out.println(anotherTrainer.getName());
+        System.out.println(anotherTrainer.getYearOfBirth());
 
-        System.out.println(anotherTrainer.getName());
-        System.out.println(anotherTrainer.getNameAndYearOfBirth());
-
-        anotherTrainer.changeName("Jhon Doe");
-        System.out.println(anotherTrainer.getName());
-
-        anotherTrainer.setName("Jack Doe");
-        System.out.println(anotherTrainer.getName());
     }
 }
