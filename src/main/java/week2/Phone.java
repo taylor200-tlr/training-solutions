1daf856e95f3e180d58c21e05ff0fc9fa253a9be
taylor200-tlr <taylor200@freemail.hu>
2020-11-07 22:33:33 +0100

    week2 konzultáció feladat

---
+++ src/main/java/week2/Phone.java
@@ -0,0 +1,33 @@
+package week2;
+
+public class Phone {
+    String type;
+    int mem;
+
+    public Phone(String type, int mem) {
+        this.type = type;
+        this.mem = mem;
+    }
+    public String getType() {
+        return type;
+    }
+
+    public int getMem() {
+        return mem;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setMem(int mem) {
+        this.mem = mem;
+    }
+
+    public static void main(String[] args) {
+        Phone telefon1 = new Phone("Nokia", 3310);
+        Phone telefon2 = new Phone("iPhone", 12);
+        System.out.println(telefon1.type + " - " + telefon1.mem);
+        System.out.println(telefon2.type + " - " + telefon2.mem);
+    }
+}
