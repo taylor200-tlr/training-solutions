7ce257e05e06dc2dbcb2bf79b8d31a678cea7951
taylor200-tlr <taylor200@freemail.hu>
2020-11-09 20:27:01 +0100

    11.05 konzultáció feladat

---
+++ src/main/java/week2/InputNames.java
@@ -0,0 +1,20 @@
+package week2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InputNames {
+    public static void main(String[] args) {
+        String [] nevek = new String[5];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i=0; i < nevek.length; i++){
+            System.out.println("Add meg a(z) " + (i+1) + ". nevet:");
+            nevek[i] = scanner.nextLine();
+        }
+        for (int j=0; j < nevek.length; j++){
+            System.out.println(nevek[j]);
+        }
+    }
+
+}
