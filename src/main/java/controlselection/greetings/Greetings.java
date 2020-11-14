36dcbdd3828caf6703569c2972088f8d92a92a52
taylor200-tlr <taylor200@freemail.hu>
2020-11-14 22:28:17 +0100

    Greetings feladat

---
+++ src/main/java/controlselection/greetings/Greetings.java
@@ -0,0 +1,21 @@
+package controlselection.greetings;
+
+public class Greetings {
+    private int hour, min;
+
+    public String greeting(int hour, int min){
+        if (hour > 4 && hour < 9){
+            return "Jó reggelt";
+        }else if (hour < 19 && min < 31){
+            return "Jó napot";
+        }else if (hour < 21){
+            return "Jó estét";
+        }
+        return "Jó éjszakát";
+    }
+
+    public static void main(String[] args) {
+        Greetings greetings = new Greetings();
+        System.out.println(greetings.greeting(19, 25));
+    }
+}
