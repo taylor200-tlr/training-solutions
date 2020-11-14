a804b91f9a5648947e66538bb26ab8115f05439a
taylor200-tlr <taylor200@freemail.hu>
2020-11-14 22:56:42 +0100

    month feladat

---
+++ src/main/java/controlselection/month/DayInMonth.java
@@ -0,0 +1,43 @@
+package controlselection.month;
+
+public class DayInMonth {
+    private int year;
+    private String month;
+
+    public int howManyDaysInMonth(int year, String month){
+        String upperedMonth = month.toUpperCase();
+        int days = 0;
+        switch (upperedMonth){
+            case "FEBRUÁR":
+                days = 28;
+                break;
+            case "ÁPRILIS":
+            case "JÚNIUS":
+            case "SZEPTEMBER":
+            case "NOVEMBER":
+                days = 30;
+                break;
+            case "JANUÁR":
+            case "MÁRCIUS":
+            case "MÁJUS":
+            case "JÚLIUS":
+            case "AUGUSZTUS":
+            case "OKTÓBER":
+            case "DECEMBER":
+                days = 31;
+                break;
+            default:
+                throw new IllegalArgumentException("Ismeretlen hónap");
+        }
+        return days;
+    }
+
+    public static void main(String[] args) {
+        DayInMonth dayInMonth = new DayInMonth();
+        System.out.println(dayInMonth.howManyDaysInMonth(2020, "Január"));
+        System.out.println(dayInMonth.howManyDaysInMonth(2020, "február"));
+        System.out.println(dayInMonth.howManyDaysInMonth(2020, "JúNiUs"));
+        System.out.println(dayInMonth.howManyDaysInMonth(2020, "oktober"));
+    }
+
+}
