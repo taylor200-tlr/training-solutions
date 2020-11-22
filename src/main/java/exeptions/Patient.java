67e3d702909bce2a2870b4e451c68f1ae0be8490
taylor200-tlr <taylor200@freemail.hu>
2020-11-22 15:14:55 +0100

    exepton patient feladat

---
+++ src/main/java/exeptions/Patient.java
@@ -0,0 +1,40 @@
+package exeptions;
+
+public class Patient {
+    private String name, socialSecutityNumber;
+    private int yearOfBirth;
+
+    public Patient(String name, String socialSecutityNumber, int yearOfBirth) {
+        if (name != null){
+            this.name = name;}
+        else throw new IllegalArgumentException("Kötelező megadni a nevet!");
+        this.socialSecutityNumber = socialSecutityNumber;
+        if (yearOfBirth >= 1900){
+            this.yearOfBirth = yearOfBirth;}
+        else throw new IllegalArgumentException("A születési év nem megfelelő!");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSocialSecutityNumber() {
+        return socialSecutityNumber;
+    }
+
+    public void setSocialSecutityNumber(String socialSecutityNumber) {
+        this.socialSecutityNumber = socialSecutityNumber;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+}
---
+++ src/main/java/typeconversion/dataloss/DataLoss.java
@@ -0,0 +1,7 @@
+package typeconversion.dataloss;
+
+public class DataLoss {
+    public static void main(String[] args) {
+
+    }
+}
