c1c4752247f20247bdaa8ee4104947e73f19adb2
taylor200-tlr <taylor200@freemail.hu>
2020-11-22 13:41:28 +0100

    Literálok feladat

---
+++ src/main/java/literals/LiteralsMain.java
@@ -0,0 +1,14 @@
+package literals;
+
+public class LiteralsMain {
+    public static void main(String[] args) {
+        double quotient = 3/4;
+        System.out.println("" + 1 + 2);
+        System.out.println("1" + "2");
+        System.out.println(quotient);
+        quotient = 3/4D;
+        System.out.println(quotient);
+        long big = 3_244_444_444L;
+    }
+
+}
