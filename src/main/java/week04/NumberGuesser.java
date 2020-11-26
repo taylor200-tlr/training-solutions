daed88ec15f09cd092770af4b9bda3ff9f77eab7
taylor200-tlr <taylor200@freemail.hu>
2020-11-26 17:31:53 +0100

    week4 Guesser feladat

---
+++ src/main/java/week04/NumberGuesser.java
@@ -0,0 +1,26 @@
+package week04;
+
+import java.util.Scanner;
+
+public class NumberGuesser {
+    public static void main(String[] args) {
+        boolean isGuessed = false;
+        System.out.println("Gondoltam egy számra, találd ki!");
+        int randomNum = (int)(Math.random() * 100 + 1);
+        System.out.println(randomNum);
+        Scanner scanner = new Scanner(System.in);
+        while (!isGuessed) {
+            System.out.println("Mi a tipped?");
+            int numberTipp = scanner.nextInt();
+            if (numberTipp > randomNum) {
+                System.out.println("Kisebbre gondoltam");
+            } else if (numberTipp < randomNum){
+                System.out.println("Nagyobbra gondoltam");
+            } else {
+                System.out.println("Gratulálok, kitaláltad!");
+                isGuessed = true;
+            }
+        }
+
+    }
+}
