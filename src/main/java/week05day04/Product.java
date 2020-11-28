d8433df341acd25aaa1b0fd7d7dc34289f7d869e
taylor200-tlr <taylor200@freemail.hu>
2020-11-28 21:49:51 +0100

    week5d04 feladat

---
+++ src/main/java/week05day04/Product.java
@@ -0,0 +1,20 @@
+package week05day04;
+
+import java.time.LocalDate;
+
+public class Product {
+    private String name;
+    private int year, month, day;
+    private LocalDate expiredDate;
+
+    public Product(String name, int year, int month, int day) {
+        this.name = name;
+        expiredDate = LocalDate.of(year, month, day);
+    }
+    public LocalDate getExpiredDate() {
+        return expiredDate;
+    }
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/week05day04/Store.java
@@ -0,0 +1,23 @@
+package week05day04;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+    List<Product> products = new ArrayList<>();
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    public int getNumberOfExpired(){
+        int result = 0;
+        for (Product product: products){
+            if (product.getExpiredDate().isBefore(LocalDate.now())){
+                result ++;
+            }
+        }
+        return result;
+    }
+
+}
