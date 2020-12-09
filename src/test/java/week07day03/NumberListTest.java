2b5468430686a6866851f35d43715684d63c32d4
taylor200-tlr <taylor200@freemail.hu>
2020-12-09 20:56:01 +0100

    Numberlist tesztesettel

---
+++ src/test/java/week07day03/NumberListTest.java
@@ -0,0 +1,19 @@
+package week07day03;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberListTest {
+    @Test
+    public void testNumberList(){
+        List <Integer> randomNumbers1 = Arrays.asList(1, 2, 2, 3, 4, 5);
+        List <Integer> randomNumbers2 = Arrays.asList(1, 2, 3, 4, 5, 4);
+        assertFalse(NumberList.isIncreasing(randomNumbers2));
+        assertTrue(NumberList.isIncreasing(randomNumbers1));
+    }
+}
