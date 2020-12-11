1304257d6cf940fc2c3027551121545aee40252c
taylor200-tlr <taylor200@freemail.hu>
2020-12-11 23:16:00 +0100

    Valami nem jó, nem fut le a teszt

---
+++ src/test/java/abstractclass/gamecharacter/CharacterTest.java
@@ -0,0 +1,26 @@
+package abstractclass.gamecharacter;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharacterTest {
+    @Test
+    public void testCharacter() {
+        Character winner;
+        Point archerPoint = new Point(1, 1);
+        Point axeWarriorPoint = new Point(18, 3);
+        Random random = new Random();
+
+        Character archer = new Archer(archerPoint, random);
+        Character warrior = new AxeWarrior(axeWarriorPoint, random);
+
+        winner = new BattleField().fight(archer, warrior);
+        System.out.println(archer.getHitPoint());
+        System.out.println(warrior.getHitPoint());
+
+    }
+
+}
