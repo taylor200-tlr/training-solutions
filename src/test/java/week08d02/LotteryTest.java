395db7e4c8f2f1d25a4f52c189f1a712ae66fa3b
taylor200-tlr <taylor200@freemail.hu>
2020-12-15 18:39:59 +0100

    3. verzió

--- src/test/java/week08d02/LotteryTest.java
+++ src/test/java/week08d02/LotteryTest.java
@@ -2,11 +2,6 @@ package week08d02;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class LotteryTest {
     @Test
     public void testLottery(){
