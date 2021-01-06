802a0b472e9afaa86a6eb3fe86ef2bf7a89a500b
taylor200-tlr <taylor200@freemail.hu>
2021-01-06 19:32:52 +0100

    exam02 első feladat kész

--- src/main/java/exam02/ArraySelector.java
+++ src/main/java/exam02/ArraySelector.java
@@ -1,6 +1,20 @@
 package exam02;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArraySelector {
 
+    public static String selectEvens(int[] array) {
+        if (array.length == 0){
+            return "";
+        }
+        List<Integer> result = new ArrayList<>();
 
+        for (int i = 0; i < array.length; i += 2) {
+            result.add(array[i]) ;
+        }
+        return result.toString();
+    }
 }
