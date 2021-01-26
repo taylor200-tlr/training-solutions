d484b4e24bb0b9b9a3b9ad21c7a7e6245512beac
taylor200-tlr <taylor200@freemail.hu>
2021-01-26 17:08:47 +0100

    kész - "Tiszavárkonyi Szőlők"

---
+++ src/main/java/week13d01/Cities.java
@@ -0,0 +1,53 @@
+package week13d01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cities {
+    private Path path;
+    private List<String> cities = new ArrayList<>();
+
+    private void readFile(Path path) {
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            while (reader.readLine() != null) {
+                splitLine(reader.readLine());
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+    }
+
+    //mivel egy irányítószámhoz több település is tartozhat
+    private String splitLine(String line) {
+        String result[] = line.split(";");
+        for (int i = 1; i < result.length; i++) {
+            cities.add(result[i]);
+        }
+        return result[1];
+    }
+
+    public String getLongestCityName() {
+        readFile(path);
+        String result = "";
+        for (String actual : cities) {
+            if (actual.length() > result.length()) {
+                result = actual;
+            }
+        }
+        return result;
+    }
+
+    public Cities(Path path) {
+        this.path = path;
+    }
+
+    public static void main(String[] args) {
+        Path path = Path.of("src", "main", "resources", "telepulesek.txt");
+        Cities cities = new Cities(path);
+        System.out.println(cities.getLongestCityName());
+    }
+}
