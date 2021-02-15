c32cef88d7a4f57c943f8794980d75f448e3d12d
taylor200-tlr <taylor200@freemail.hu>
2021-02-15 21:51:27 +0100

    kész

--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -7,25 +7,59 @@ import java.nio.file.Path;
 
 public class Histogram {
 
-    private Path path = Path.of("histogram.txt");
+// Első nekifutás
+//    private Path path = Path.of("histogram.txt");
+//
+//    public void readLine(Path path) {
+//        String line;
+//        try (BufferedReader reader = Files.newBufferedReader(path)) {
+//            while ((line = reader.readLine()) != null) {
+//                createHistogram(line);
+//            }
+//        } catch (IOException ioe) {
+//            throw new IllegalStateException("Can not read file!", ioe);
+//        }
+//    }
+//
+//    public void createHistogram(String line) {
+//        int result = Integer.parseInt(line);
+//        for (int i = 0; i < result; i++) {
+//            System.out.print('*');
+//        }
+//        System.out.print("" + "\n");
+//    }
 
-    public void readLine(Path path) {
+// Második nekifutás StringBuilderrel
+
+    public String createHistogram(BufferedReader reader) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        String result = "";
         String line;
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-            while ((line = reader.readLine()) != null) {
-                createHistogram(line);
-            }
-        } catch (IOException ioe) {
-            throw new IllegalStateException("Can not read file!", ioe);
+        int numberInLine;
+        while ((line = reader.readLine())  != null) {
+//            System.out.println(line);
+            numberInLine = Integer.parseInt(line);
+            result = drawHistogram(numberInLine, '*');
+            stringBuilder.append(result);
+            stringBuilder.append("\n");
         }
+        return stringBuilder.toString();
     }
 
-    public void createHistogram(String line) {
-        int result = Integer.parseInt(line);
-        for (int i = 0; i < result; i++) {
-            System.out.print('*');
+    private String drawHistogram(int numberInLine, char c) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i=0; i<numberInLine;i++){
+            stringBuilder.append(c);
         }
-        System.out.print("" + "\n");
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = Files.newBufferedReader(Path.of("histogram.txt"))) {
+            System.out.println(new Histogram().createHistogram(reader));
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
     }
 
 }
--- src/test/java/exam03/HistogramTest.java
+++ src/test/java/exam03/HistogramTest.java
@@ -1,16 +1,44 @@
+//package exam03;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import java.nio.file.Path;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class HistogramTest {
+//    Histogram histogram = new Histogram();
+//
+//    @Test
+//    void createHistogram() {
+//        histogram.readLine(Path.of("histogram.txt"));
+//    }
+//}
+
 package exam03;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class HistogramTest {
-    Histogram histogram = new Histogram();
+public class HistogramTest {
 
     @Test
-    void createHistogram() {
-        histogram.readLine(Path.of("histogram.txt"));
+    void createHistogram() throws IOException {
+//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(HistogramTest.class.getResourceAsStream("histogram.txt")))) {
+        try (BufferedReader reader = Files.newBufferedReader(Path.of("histogram.txt"))) {
+            String result = new Histogram().createHistogram(reader);
+//            System.out.println(result);
+            assertEquals("***\n" +
+                    "****\n" +
+                    "**\n" +
+                    "*\n", result);
+        }
     }
 }
