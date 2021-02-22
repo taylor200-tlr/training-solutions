bc0eaaab5f6edf8e892f987647d0e35eed445233
taylor200-tlr <taylor200@freemail.hu>
2021-02-22 21:00:14 +0100

    CdvCheck kész (vacsora és fejfájáscsillapító után)

--- src/main/java/exam03retake01/OwlCounter.java
+++ src/main/java/exam03retake01/OwlCounter.java
@@ -3,6 +3,7 @@ package exam03retake01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,6 +11,19 @@ import java.util.Map;
 public class OwlCounter {
 
     private Map<String, Integer> owlsByRegion = new HashMap<>();
+
+    public void readFile(Path path){
+        String[] dataLine;
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                dataLine = line.split("=");
+                owlsByRegion.put(dataLine[0], Integer.parseInt(dataLine[1]));
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException("File not found", e);
+        }
+    }
 
     public void readFromFile(BufferedReader reader) throws IOException {
         String line;
@@ -23,5 +37,13 @@ public class OwlCounter {
 
     public int getNumberOfOwls(String region) {
         return owlsByRegion.get(region);
+    }
+
+    public static void main(String[] args) {
+        OwlCounter owlCounter = new OwlCounter();
+        Path path = Path.of("owls.txt");
+        owlCounter.readFile(path);
+        System.out.println(owlCounter.getNumberOfOwls("Baranya"));
+        System.out.println(Character.isDigit('c'));
     }
 }
